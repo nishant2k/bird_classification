3287eb3654d59c0f977c34b8fa29dd04ee23d730
Nishant Vedwal <56424858+nishant2k@users.noreply.github.com>
2020-05-14 21:52:49 +0530

    Add files via upload

---
+++ make_file.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 15:00:57 2020
+
+@author: 91811
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 13:27:23 2020
+
+@author: 91811
+"""
+
+import pandas as pd
+import os
+
+files = ["1", "P03929TEA1_20170505_113500.Table.1.selections", "P03929TEA1_20170505_190500.Table.1.selections", "P03929TEA1_20170506_070001.Table.1.selections",
+         "P03929TEA1_20170506_143500.Table.1.selections", "P03929TEA1_20170506_193500.Table.1.selections", "P03929TEA1_20170507_072002.Table.1.selections",
+         "P03929TEA1_20170507_120500.Table.1.selections", "P03929TEA1_20170507_203500.Table.1.selections", "P03929TEA1_20170508_062900.Table.1.selections",
+         "P03929TEA1_20170508_120400.Table.1.selections", "P03929TEA1_20170508_190400.Table.1.selections", "P03929TEA1_20170509_061900.Table.1.selections", "P03929TEA1_20170509_123400.Table.1.selections",
+         "P03929TEA1_20170509_200400.Table.1.selections", "P03929TEA1_20170510_060400.Table.1.selections", "P03929TEA1_20170510_143400.Table.1.selections", "P03929TEA1_20170510_203400.Table.1.selections",
+         "P03929TEA1_20170511_064801.Table.1.selections", "P03929TEA1_20170511_143300.Table.1.selections", "P03929TEA1_20170511_190300.Table.1.selections", "P03929TEA1_20170512_064801.Table.1.selections",
+         "P03932FOR1_20170505_064101.Table.1.selections", "P03932FOR1_20170505_110600.Table.1.selections", "P03932FOR1_20170505_200600.Table.1.selections", "P03932FOR1_20170508_060500.Table.1.selections",
+         "P03932FOR1_20170508_130500.Table.1.selections", "P03932FOR1_20170508_193500.Table.1.selections", "P03932FOR1_20170511_075403.Table.1.selections", "P03932FOR1_20170511_143400.Table.1.selections", 
+         "P03932FOR1_20170511_193400.Table.1.selections", "P03932FOR1_20170512_070302.Table.1.selections", "P03939FOR2_20170505_072502.Table.1.selections", "P03939FOR2_20170505_130500.Table.1.selections",
+         "P03939FOR2_20170505_190500.Table.1.selections", "P03939FOR2_20170506_075503.Table.1.selections", "P03939FOR2_20170506_133500.Table.1.selections", "P03939FOR2_20170506_200500.Table.1.selections",
+         "P03939FOR2_20170507_070502.Table.1.selections", "P03939FOR2_20170507_143500.Table.1.selections", "P03939FOR2_20170507_200500.Table.1.selections", "P03939FOR2_20170508_072902.Table.1.selections",
+         "P03939FOR2_20170508_113400.Table.1.selections", "P03939FOR2_20170508_200400.Table.1.selections", "P03939FOR2_20170509_062400.Table.1.selections", "P03939FOR2_20170509_133400.Table.1.selections",
+         "P03939FOR2_20170509_193400.Table.1.selections", "P03939FOR2_20170510_061400.Table.1.selections", "P03939FOR2_20170510_120400.Table.1.selections", "P03939FOR2_20170510_190400.Table.1.selections",
+         "P03939FOR2_20170511_064301.Table.1.selections", "P03939FOR2_20170511_133300.Table.1.selections", "P03939FOR2_20170511_193300.Table.1.selections", "P03939FOR2_20170512_072802.Table.1.selections",
+         "P03939FOR2_20170512_130300.Table.1.selections", "P03940TEA2_20170505_061500.Table.1.selections", "P03940TEA2_20170505_133500.Table.1.selections", "P03940TEA2_20170505_193500.Table.1.selections" 
+         ]
+##extraxt filed as csv
+#path = "C:/Users/91811/Downloads/Selections-20200514T132501Z-001/Selections"
+#for i in range(len(files)):
+#  a=pd.read_csv(path+"/"+files[i]+".txt", sep="\t")
+#  print(a["Begin Time (s)"])
+##converting txt files to csv files
+#
+#  a.to_csv("/content/csv_file"+"/"+files[i]+".csv")
+#  #b=pd.read_csv("/content/csv_file/a.csv")
+# # b["Diff"] = b["End Time (s)"] - b["Begin Time (s)"]
+#
+##b1 = set(a["Notes"]) # #created a set of all species,no repetition
+#
+##m=list(b1)
+##k=b1.add("Ikkkkk")
+##n=list(b1)
+##print(len(n))
+##l="kkkkk"
+##if l not in n:
+#  #n.append(l)
+### pass
+#
+##print(len(n))
+#print(len(k))
+a1 = pd.read_csv("C:/Users/91811/Downloads/Selections-20200514T132501Z-001/Selections2/1.csv")
+a1["Diff"] = a1["End Time (s)"] - a1["Begin Time (s)"] # create new column for differerbce
+
+b1 = list(set(a1["Notes"])) #created a set of all species,no repetition
+
+# define the access rights
+access_rights = 0o755
+
+path1 = "C:/Users/91811/Downloads/Selections-20200514T132501Z-001/S3/bird"
+
+#make bird directory
+try:
+    os.mkdir(path1,access_rights)    
+except FileExistsError:
+    print("Directory already exists")
+
+
+#making directories
+for i in range(len(b1)):
+    path2=path1+"/"+str(b1[i])
+    try:
+        os.mkdir(path2,access_rights)    
+    except FileExistsError:
+        print("Directory already exists")
+        continue
+
+#grouping the dataframe according to the bird name code
+c=a1.groupby("Notes")
+for i in range(len(b1)):
+    d=c.get_group(b1[i])
+    path2=path1+"/"+str(b1[i])+"/finally.csv"
+    e=d.to_csv(path2)  #exporting the final csv into their respective folders
+
+for i in range(1,len(files)):
+  df = pd.read_csv("C:/Users/91811/Downloads/Selections-20200514T132501Z-001/Selections2"+"/"+files[i]+".csv")
+  df["Diff"] = df["End Time (s)"] - df["Begin Time (s)"]
+  b = df["Notes"]
+  for j in range(len(b)):
+    if str(b[j]) not in b1:
+      b1.append(b[j])
+      path3 = path1+"/"+str(b[j])
+      try:
+          os.mkdir(path3,access_rights)    
+      except FileExistsError:
+          print("Directory already exists")
+          continue
+      c11 = df.groupby("Notes")
+      #for k in range(len(b1)):
+      d22 = c11.get_group(b[j])
+      path4 = path3+"/"+"finally.csv"
+      e1=d22.to_csv(path4)  #exporting the final csv into their respective folders
+    else:
+      pass
