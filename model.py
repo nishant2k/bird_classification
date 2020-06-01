71379bc78bc49463c9face3030c0e978d1660d77
Nishant Vedwal <56424858+nishant2k@users.noreply.github.com>
2020-06-01 14:10:20 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,11 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May 20 20:28:48 2020
-
-@author: nishant
-"""
-
+"""Importing necessery libraries"""
 import os
 import numpy as np
 import pandas as pd
@@ -20,32 +13,22 @@ from tensorflow.keras.optimizers import Adam
 
 from sklearn.model_selection import train_test_split
 
+"""Function for building our model"""
 def build_model(size, num_classes):
     inputs = Input((size, size, 3))
-    backbone = MobileNetV2(input_tensor=inputs, include_top=False, weights="imagenet")
+    backbone = MobileNetV2(input_tensor=inputs, include_top=False, weights="imagenet") #Using mobilenetV2 for our model
     backbone.trainable = True
     x = backbone.output
     x = GlobalAveragePooling2D()(x)
     x = Dropout(0.2)(x)
     x = Dense(1024, activation="relu")(x)
-    x = Dense(num_classes, activation="softmax")(x)
     
-#    x = GlobalAveragePooling2D()(x)
-#    x = Dropout(0.2)(x)
-#    x = Dense(1024, activation="relu")(x)
-#   #x = Dense(num_classes, activation="softmax")(x)
-#    
-#    
-#    x = GlobalAveragePooling2D()(x)
-#    x = Dropout(0.2)(x)
-#    x = Dense(1024, activation="relu")(x)
-#    x=Dense(num_classes, activation="softmax")(x)
-
 
 
     model = tf.keras.Model(inputs, x)
     return model
 
+"""Function for reading the images"""
 def read_image(path, size):
     image = cv2.imread(path, cv2.IMREAD_COLOR)
     image = cv2.resize(image, (size, size))
@@ -81,95 +64,58 @@ def tf_dataset(x, y, batch=8):
     return dataset
 
 
-path = "/home/nishant/Documents/bird_f"
-train_path = os.path.join(path, "train/*")
-test_path = os.path.join(path, "test/*")
-labels_path = os.path.join(path, "labels.csv")
+path = "/content/bird_f" # path to the directory conatining the train and test datasets
+train_path = os.path.join(path, "train/*") #path of train dataset
+#test_path = os.path.join(path, "test/*")
+labels_path = os.path.join(path, "train_labels.csv") # path for the labels of train data conating images with their respective bird name
 
 
 labels_df = pd.read_csv(labels_path)
-breed = labels_df["breed"].unique()
-print("Number of Breed: ", len(breed))
+breed = labels_df["breed"].unique() # creating a breed named list that will be conating the unique birds
+print("Number of Breed: ", len(breed)) 
 
-breed2id = {name: i for i, name in enumerate(breed)}
+breed2id = {name: i for i, name in enumerate(breed)} # Creating a Dictionary that will conatin breed name with respective make up id
+id2breed = {i: name for i, name in enumerate(breed)} # reverse of breed2id
 
-ids = glob(train_path)
+
+ids = glob(train_path) # conating the train path randomly
 labels = []
 
 for image_id in ids:
     image_id = image_id.split("/")[-1].split(".")[0]
-    breed_name = list(labels_df[labels_df.id == image_id]["breed"])[0]
-    breed_idx = breed2id[breed_name]
-    labels.append(breed_idx)
+    breed_name = list(labels_df[labels_df.id == image_id]["breed"])[0] # breed name will be name of the bird
+    breed_idx = breed2id[breed_name] #converting breed name to index
+    labels.append(breed_idx) # appending the breed index to labels list
+
+train_x, valid_x = train_test_split(ids, test_size=0.2, random_state=42) # spliting the data for validation
+train_y, valid_y = train_test_split(labels, test_size=0.2, random_state=42)
 
 
-labels_path2 = os.path.join(path, "labels_t.csv")
-
-
-labels_df2 = pd.read_csv(labels_path2)
-breed2 = labels_df2["breed"].unique()
-print("Number of Breed: ", len(breed2))
-
-breed2id2 = {name: j for j, name in enumerate(breed2)}
-
-ids2 = glob(test_path)
-labels2 = []
-
-for image_id in ids2:
-    image_id = image_id.split("/")[-1].split(".")[0]
-    breed_name2 = list(labels_df2[labels_df2.id == image_id]["breed"])[0]
-    breed_idx2 = breed2id2[breed_name2]
-    labels2.append(breed_idx2)
-
-#ids = ids[:1000]
-#labels = labels[:1000]
-
-    ## Spliting the dataset
-#train_x, valid_x = train_test_split(ids, test_size=0.2, random_state=42)
-#train_y, valid_y = train_test_split(labels, test_size=0.2, random_state=42)
-train_x = ids
-train_y = labels
-valid_x = ids2
-valid_y = labels2
-zip1 =list(zip(train_x,train_y))
-zip2 = list(zip(valid_x,valid_y))
-
-random.shuffle(zip1)
-random.shuffle(zip2)
-
-res1= list(zip(*zip1))
-
-res2= list(zip(*zip2))
-
-train_x = list(res1[0])
-train_y = list(res1[1])
-valid_x = list(res2[0])
-valid_y = list(res2[1])
-
-
-    ## Parameters
 size = 224
-num_classes = 211
-lr = 1e-4
+num_classes = 211 # numbers of birds
+lr = 1e-5 # learning rate of our model
 batch = 16
-epochs = 10
+epochs = 20
 
     ## Model
+#Creating the model with function build above
 model = build_model(size, num_classes)
 model.compile(loss="categorical_crossentropy", optimizer=Adam(lr), metrics=["acc"])
     # model.summary()
 
     ## Dataset
+    #Building the train and vaalid dataset
 train_dataset = tf_dataset(train_x, train_y, batch=batch)
 valid_dataset = tf_dataset(valid_x, valid_y, batch=batch)
 
-    ## Training
+    ## Training of the model
 callbacks = [
-        ModelCheckpoint("model.h5", verbose=1, save_best_only=True),
+        ModelCheckpoint("model.h5", verbose=1, save_best_only=True), 
         ReduceLROnPlateau(factor=0.1, patience=5, min_lr=1e-6)
-        ]
+        ] # saving our model as model.h5
 train_steps = (len(train_x)//batch) + 1
 valid_steps = (len(valid_x)//batch) + 1
+#training the model
 model.fit(train_dataset,
         steps_per_epoch=train_steps,
         validation_steps=valid_steps,
